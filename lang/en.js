0ace793f24bb48e98dc944ebea9c3b7d0a60ca1a
Keren Meltzer <keren@basistech.com>
2014-12-07 20:46:41 +0200

    complete hebrew translation

--- lang/en.js
+++ lang/en.js
@@ -10,4 +10,10 @@
    , 'about_paragragh' : "Granny's Feed is here to connect your desperately technophobic granny to the internet, using any second hand basic computer. Share family photos and make video calls with her, she doesn't need to know anything about computers."
    , 'diy_label' : 'Do It Yourself Guide'
    , 'diy_paragraph' : 'Eight easy steps for configuring Skype, Dropbox, and your operating system to provide your Granny with all this goodness.'
+   , 'app-coming_label' : 'The chrome app is Coming Soon!'
+   , 'app-coming_paragragh' : "Granny's Feed is a project developed at She Code(tlv); we are working in our free time to make as many grannies happy by coding a free easy to install application that will turn any computer to granny's computer in seconds."
+   , 'wait-list_label' : 'Join the Wait List'
+   , 'wait-list_paragragh' : "If your granny can't wait till Granny's Feed is out, drop us a line and we'll let you know when that happens."
+   , 'contact-us_label' : 'Contact Us'
+   , 'contact-us_paragragh' : "We would love to hear from you, answer any question and get your feedback.Michal Yashpe & Keren Meltzer."
  }
